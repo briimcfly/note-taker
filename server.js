cd7e75c04da7452b2f9b5eb68206b75532fc171c
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-08-29 14:08:24 -0700

    writeFile POSTS are working and storing to db.json

--- server.js
+++ server.js
@@ -24,6 +24,37 @@ app.get('/api/notes', (req, res) => {
 //PUT Requests for Notes
 app.post('/api/notes', (req, res) => {
     res.status(200).json(`${req.method} request received to get notes`);
+
+    //Destructure req.body
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            noteID: uuidv4()
+        };
+    
+    //Obtain existing Notes
+    readFile('db/db.json', 'utf8', (err, data)=>{
+        if (err) {
+            console.error(err);
+        } else {
+        //Convert JSON
+        const parsedNotes = JSON.parse(data);
+
+        //Add a new Note 
+        parsedNotes.push(newNote);
+
+        //Write updated Notes back to File with 4 spaces
+        writeFile(
+            'db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr)=> writeErr ? console.error(writeErr) : console.info('Updated Notes')
+            );
+        }
+    })
+    };
 })
 
 app.listen(port, () => {
